0870d46584ebb4d8ad16bbdd96af8bfa4dbc4fa1
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-11-14 12:19:12 +0800

    merge learning and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import RenderingList2 from './components/RenderingList2'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const myFruits = [
-    {
-      name: 'banana',
-      calories: '95',
-    },
-    {
-      name: 'strawberry',
-      calories: '105',
-    },
-  ]
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <RenderingList2 fruits={myFruits} />
+      <div>
+        <a href='https://vite.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className='card'>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
